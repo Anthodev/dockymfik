05dac5394fcde93ef9ab1b2e98f1029dc5b2c7d3
Cedric Anthony <dev+git@anthodev.net>
2019-11-01 22:26:21 +0100

    Added creation of the docker network if not existing

--- setup.sh
+++ setup.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+if ! docker network ls | grep -q 'dockymfik-network'; then
+    echo "Creating a docker network for this tool"
+    docker network create dockymfik-network
+    echo "Docker network created"
+fi
+
 docker-compose -f ./docker-network/docker-compose.yml build
 docker-compose -f ./docker-app/docker-compose.yml build
